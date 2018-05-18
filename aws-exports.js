3996ffbf6bd5a61b72752b4535e882429afe03cc
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-05-18 13:59:33 -0700

    updated aws-exports

--- aws-exports.js
+++ aws-exports.js
@@ -1,6 +1,6 @@
 // AWS Mobile Hub Project Constants
 const awsmobile = {
-    'aws_appsync_graphqlEndpoint': 'YOURENDPOING',
+    'aws_appsync_graphqlEndpoint': 'YOURENDPOINT',
     'aws_appsync_region': 'YOURREGION',
     'aws_appsync_authenticationType': 'API_KEY',
     'aws_appsync_apiKey': 'YOURAPIKEY'
